5412a9a703cb8ecd9e5da0247c13a6caecfd3d59
Micaheal Johnston <johnston.michaeld@yahoo.com>
2023-03-16 12:51:59 -0600

    navbar element finally working

--- components.js
+++ components.js
@@ -1,20 +1,67 @@
 class Navbar extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-      <nav class="navbar">
-        <a href="#" class="logo">${this.getAttribute("logo")}</a>
-        <div class="navbar-items" color="white">
-        <nav class="navbar">
-        <a href="#" class="logo">Logo</a>
-        <div class="navbar-items">
-          <a href="#">Home</a>
-          <a href="#">About</a>
-          <a href="#">Contact</a>
+    <section id="navbar" data-bs-theme="dark">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="index.html">Catacombs</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="index.html"
+                >Home</a
+              >
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                the Crawlspace
+              </a>
+              <ul class="dropdown-menu">
+                <li>
+                  <a class="dropdown-item" href="stories.html">the Campfire</a>
+                </li>
+                <li><a class="dropdown-item" href="#">Body Bags</a></li>
+                <li><hr class="dropdown-divider" /></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled">Disabled</a>
+            </li>
+          </ul>
+          <form class="d-flex" role="search">
+            <input
+              class="form-control me-2"
+              type="search"
+              placeholder="Search, Not Yet"
+              aria-label="Search"
+            />
+            <button class="btn btn-outline-light" type="submit">Search</button>
+          </form>
         </div>
-      </nav>
-      
-        </div>
-      </nav>
+      </div>
+    </nav>
+  </section>
     `;
   }
 }
